76d2673e43387e4ff95f3037e0a422939b1e38f6
Nguyen Thien Phuoc <23130252@st.hcmuaf.edu.vn>
2026-04-15 23:48:21 +0700

    change

--- GildeRose/java/src/main/java/com/gildedrose/GildedRose.java
+++ GildeRose/java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,21 +1,15 @@
 package com.gildedrose;
 
-public class Item {
+class GildedRose {
+    Item[] items;
 
-    public String name;
-
-    public int sellIn;
-
-    public int quality;
-
-    public Item(String name, int sellIn, int quality) {
-        this.name = name;
-        this.sellIn = sellIn;
-        this.quality = quality;
+    public GildedRose(Item[] items) {
+        this.items = items;
     }
 
-    @Override
-    public String toString() {
-        return this.name + ", " + this.sellIn + ", " + this.quality;
+    public void updateQuality() {
+        for (Item item : items) {
+            ItemUpdaterFactory.getUpdater(item.name).update(item);
+        }
     }
 }
